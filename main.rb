595e1e7eeddf253d060333546b7e2cd8e96d5528
Johnny Gomez <angrychinese77@yahoo.com>
2013-11-23 13:05:27 -0800

    Setup gem and db migrate

---
+++ main.rb
@@ -0,0 +1,30 @@
+# encoding: UTF-8
+require 'json'
+require 'sinatra'
+require 'data_mapper'
+require 'dm-migrations'
+
+configure :development do
+    DataMapper::Logger.new($stdout, :debug)
+
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/mydb.db")
+    # DataMapper.setup(
+    #     :default,
+    #     'mysql:://root:12345@localhost/sinatra_service'
+    # )
+end
+
+
+configure :production do
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/mydb.db")
+    # DataMapper.setup(
+    #     :default,
+    #     'postgres:://postgres:12345@localhost/sinatra_service'
+    # )
+end
+
+# require './models/init'
+# require './models/init'
+# require './models/init'
+
+DataMapper.finalize
