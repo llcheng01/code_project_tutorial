91ec620e79db185706f00a4a78b3df56c720a01c
Johnny Cheng <johnny.cheng7b9@gmail.com>
2014-08-11 08:57:31 -0700

    Finally get angular proxy working

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -71,18 +71,37 @@ module.exports = function (grunt) {
         hostname: 'localhost',
         livereload: 35729
       },
+      proxies: [
+          {
+            context: '/api',
+            host: 'localhost',
+            port: 3000
+      }
+      ],
       livereload: {
         options: {
           open: true,
-          middleware: function (connect) {
-            return [
-              connect.static('.tmp'),
-              connect().use(
+          middleware: function (connect, options) {
+              if (!Array.isArray(options.base)) {
+                  options.base = [options.base];
+              }
+
+              // Setup the proxy
+              var middlewares = [
+                require('grunt-connect-proxy/lib/utils').proxyRequest,
+                connect.static('.tmp'),
+                connect().use(
                 '/bower_components',
-                connect.static('./bower_components')
-              ),
-              connect.static(appConfig.app)
-            ];
+                  connect.static('./bower_components')
+                ),
+                connect.static(appConfig.app)
+              ];
+
+              // Make directory browse-able
+              var directory = options.directory || options.base[options.base.length -1];
+              middlewares.push(connect.directory(directory));
+
+              return middlewares;
           }
         }
       },
@@ -402,6 +421,7 @@ module.exports = function (grunt) {
       'wiredep',
       'concurrent:server',
       'autoprefixer',
+      'configureProxies',
       'connect:livereload',
       'watch'
     ]);
@@ -416,6 +436,7 @@ module.exports = function (grunt) {
     'clean:server',
     'concurrent:test',
     'autoprefixer',
+      'configureProxies',
     'connect:test',
     'karma'
   ]);
